dc490eac12528165d00269edc3b659f2c2b1c7f9
aliakbarnezhad3535 <aliakbarnezhad35@gmail.com>
2026-08-23 05:10:52 -0700

    Add bot cod

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+import os
+import telebot
 
+TOKEN = os.environ["BOT_TOKEN"]
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=["start"])
+def start(message):
+    bot.reply_to(
+        message,
+        "سلام 👋\n"
+        "به ربات Fire VPN خوش اومدی 🔥\n\n"
+        "🛒 خرید کانفیگ\n"
+        "💰 تعرفه‌ها\n"
+        "📞 پشتیبانی"
+    )
+
+bot.infinity_polling()
